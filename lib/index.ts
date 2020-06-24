202edd847e32a4578ec47a1a500d80b6b8372529
Madis Kariler <madis.kariler@gmail.com>
2020-06-24 14:46:15 +0300

    Omit try-catch

--- lib/index.ts
+++ lib/index.ts
@@ -82,156 +82,151 @@ export default function (opts = defaultOptions): GenericCallHandler {
     const logger = opts.logger;
     const logConf: CallLogConfiguration | undefined = opts.logConfigurations[call.ctx.method.path];
 
-    try {
-      const startTime = Date.now();
+    const startTime = Date.now();
 
-      // Collect basic call info
-      const callInfo: CallInfo = {
-        path: call.ctx.method.path,
-        call_type: 'unary',
-        message: '',
-      };
+    // Collect basic call info
+    const callInfo: CallInfo = {
+      path: call.ctx.method.path,
+      call_type: 'unary',
+      message: '',
+    };
 
-      // If the 'req-id' middleware is used, attach the generated
-      // id to our log message
-      if (call.ctx.locals.reqId) {
-        callInfo.req_id = call.ctx.locals.reqId as string;
+    // If the 'req-id' middleware is used, attach the generated
+    // id to our log message
+    if (call.ctx.locals.reqId) {
+      callInfo.req_id = call.ctx.locals.reqId as string;
+    }
+
+    if (call.ctx.method.requestStream && call.ctx.method.responseStream) {
+      callInfo.call_type = 'bidi_stream';
+    } else if (call.ctx.method.requestStream) {
+      callInfo.call_type = 'client_stream';
+    } else if (call.ctx.method.responseStream) {
+      callInfo.call_type = 'server_stream';
+    }
+
+    const startMsg: StartMsg = {
+      ...callInfo,
+      message: 'Start call handling',
+    };
+
+    if (!logConf || !logConf.omitRequestMetadata) {
+      startMsg.metadata = call.core.metadata;
+    }
+
+    if (!call.ctx.method.requestStream) {
+      call = call as
+        | ChainServerUnaryCall<jspb.Message, jspb.Message>
+        | ChainServerWritableStream<jspb.Message, jspb.Message>;
+
+      if (!logConf || !logConf.omitUnaryRequestPayload) {
+        const payload = call.req.toObject();
+        if (logConf && logConf.omitUnaryRequestPayloadKeys) {
+          for (const prop of logConf.omitUnaryRequestPayloadKeys) {
+            omitProperty(payload, prop);
+          }
+        }
+        startMsg.payload = payload;
       }
+    }
 
-      if (call.ctx.method.requestStream && call.ctx.method.responseStream) {
-        callInfo.call_type = 'bidi_stream';
-      } else if (call.ctx.method.requestStream) {
-        callInfo.call_type = 'client_stream';
-      } else if (call.ctx.method.responseStream) {
-        callInfo.call_type = 'server_stream';
-      }
+    if (!call.ctx.method.responseStream) {
+      call = call as
+        | ChainServerReadableStream<jspb.Message, jspb.Message>
+        | ChainServerUnaryCall<jspb.Message, jspb.Message>;
 
-      const startMsg: StartMsg = {
-        ...callInfo,
-        message: 'Start call handling',
-      };
+      call.onUnaryDataSent((payloadPb) => {
+        const finishMsg: FinishMsg = {
+          ...callInfo,
+          message: 'Call handling finished',
+          took_ms: startTime - Date.now(),
+        };
 
-      if (!logConf || !logConf.omitRequestMetadata) {
-        startMsg.metadata = call.core.metadata;
-      }
-
-      if (!call.ctx.method.requestStream) {
-        call = call as
-          | ChainServerUnaryCall<jspb.Message, jspb.Message>
-          | ChainServerWritableStream<jspb.Message, jspb.Message>;
-
-        if (!logConf || !logConf.omitUnaryRequestPayload) {
-          const payload = call.req.toObject();
-          if (logConf && logConf.omitUnaryRequestPayloadKeys) {
-            for (const prop of logConf.omitUnaryRequestPayloadKeys) {
+        if (!logConf || !logConf.omitUnaryResponsePayload) {
+          const payload = payloadPb.toObject();
+          if (logConf && logConf.omitUnaryResponsePayloadKeys) {
+            for (const prop of logConf.omitUnaryResponsePayloadKeys) {
               omitProperty(payload, prop);
             }
           }
-          startMsg.payload = payload;
+          finishMsg.payload = payload;
         }
-      }
 
-      if (!call.ctx.method.responseStream) {
-        call = call as
-          | ChainServerReadableStream<jspb.Message, jspb.Message>
-          | ChainServerUnaryCall<jspb.Message, jspb.Message>;
-
-        call.onUnaryDataSent((payloadPb) => {
-          const finishMsg: FinishMsg = {
-            ...callInfo,
-            message: 'Call handling finished',
-            took_ms: startTime - Date.now(),
-          };
-
-          if (!logConf || !logConf.omitUnaryResponsePayload) {
-            const payload = payloadPb.toObject();
-            if (logConf && logConf.omitUnaryResponsePayloadKeys) {
-              for (const prop of logConf.omitUnaryResponsePayloadKeys) {
-                omitProperty(payload, prop);
-              }
-            }
-            finishMsg.payload = payload;
-          }
-
-          logger.info(finishMsg);
-        });
-      }
-
-      if (call.ctx.method.requestStream) {
-        call = call as
-          | ChainServerReadableStream<jspb.Message, jspb.Message>
-          | ChainServerDuplexStream<jspb.Message, jspb.Message>;
-
-        call.onInStreamEnded(() => {
-          const msg: StreamMsg = {
-            ...callInfo,
-            message: 'Inbound stream has ended',
-          };
-          logger.info(msg);
-        });
-
-        call.onMsgIn((payloadPb, nextGate) => {
-          nextGate();
-
-          const streamMsg: StreamDataMsg = {
-            ...callInfo,
-            message: 'Received data from peer via stream',
-          };
-
-          if (!logConf || !logConf.omitStreamInMsgPayload) {
-            const payload = payloadPb.toObject();
-            if (logConf && logConf.omitStreamInMsgPayloadKeys) {
-              for (const prop of logConf.omitStreamInMsgPayloadKeys) {
-                omitProperty(payload, prop);
-              }
-            }
-            streamMsg.payload = payload;
-          }
-
-          logger.info(streamMsg);
-        });
-      }
-
-      if (call.ctx.method.responseStream) {
-        call = call as
-          | ChainServerWritableStream<jspb.Message, jspb.Message>
-          | ChainServerDuplexStream<jspb.Message, jspb.Message>;
-
-        call.onOutStreamEnded(() => {
-          const msg: StreamMsg = {
-            ...callInfo,
-            message: 'Outbound stream has ended',
-          };
-          logger.info(msg);
-        });
-
-        call.onMsgOut((payloadPb, nextGate) => {
-          nextGate();
-
-          const streamMsg: StreamDataMsg = {
-            ...callInfo,
-            message: 'Sending data to peer via stream',
-          };
-
-          if (!logConf || !logConf.omitStreamOutMsgPayload) {
-            const payload = payloadPb.toObject();
-            if (logConf && logConf.omitStreamOutMsgPayloadKeys) {
-              for (const prop of logConf.omitStreamOutMsgPayloadKeys) {
-                omitProperty(payload, prop);
-              }
-            }
-            streamMsg.payload = payload;
-          }
-
-          logger.info(streamMsg);
-        });
-      }
-
-      logger.info(startMsg);
-    } catch (err) {
-      logger.error('Call logging failed');
-      logger.error(err.toString());
+        logger.info(finishMsg);
+      });
     }
+
+    if (call.ctx.method.requestStream) {
+      call = call as
+        | ChainServerReadableStream<jspb.Message, jspb.Message>
+        | ChainServerDuplexStream<jspb.Message, jspb.Message>;
+
+      call.onInStreamEnded(() => {
+        const msg: StreamMsg = {
+          ...callInfo,
+          message: 'Inbound stream has ended',
+        };
+        logger.info(msg);
+      });
+
+      call.onMsgIn((payloadPb, nextGate) => {
+        nextGate();
+
+        const streamMsg: StreamDataMsg = {
+          ...callInfo,
+          message: 'Received data from peer via stream',
+        };
+
+        if (!logConf || !logConf.omitStreamInMsgPayload) {
+          const payload = payloadPb.toObject();
+          if (logConf && logConf.omitStreamInMsgPayloadKeys) {
+            for (const prop of logConf.omitStreamInMsgPayloadKeys) {
+              omitProperty(payload, prop);
+            }
+          }
+          streamMsg.payload = payload;
+        }
+
+        logger.info(streamMsg);
+      });
+    }
+
+    if (call.ctx.method.responseStream) {
+      call = call as
+        | ChainServerWritableStream<jspb.Message, jspb.Message>
+        | ChainServerDuplexStream<jspb.Message, jspb.Message>;
+
+      call.onOutStreamEnded(() => {
+        const msg: StreamMsg = {
+          ...callInfo,
+          message: 'Outbound stream has ended',
+        };
+        logger.info(msg);
+      });
+
+      call.onMsgOut((payloadPb, nextGate) => {
+        nextGate();
+
+        const streamMsg: StreamDataMsg = {
+          ...callInfo,
+          message: 'Sending data to peer via stream',
+        };
+
+        if (!logConf || !logConf.omitStreamOutMsgPayload) {
+          const payload = payloadPb.toObject();
+          if (logConf && logConf.omitStreamOutMsgPayloadKeys) {
+            for (const prop of logConf.omitStreamOutMsgPayloadKeys) {
+              omitProperty(payload, prop);
+            }
+          }
+          streamMsg.payload = payload;
+        }
+
+        logger.info(streamMsg);
+      });
+    }
+
+    logger.info(startMsg);
 
     next();
   };
